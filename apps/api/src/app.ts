ac85aead8b8b8dab75746d469219e74d3ca6d9aa
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2022-07-13 16:08:18 -0400

    api: add helmet and chors middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,8 +2,13 @@ import express from 'express'
 import morgan from 'morgan'
 import { v1Routes } from '@src/routes/v1/index.route'
 import { errorHandler } from './middleware/errorHandler'
+import cors from 'cors'
+import helmet from 'helmet'
 
 export const app = express()
+
+app.use(cors())
+app.use(helmet())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
